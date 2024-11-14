0c083acd7b1de6f01eaf90aedc64f661ed97d178
Abdelbadia KRAIEM <Abdelbadia.KRAIEM@hr-path.com>
2024-11-14 15:42:03 +0100

    modif build4

--- .github/workflows/prebuild.js
+++
@@ -1,6 +0,0 @@
-// prebuild.js
-process.env.NODE_OPTIONS = '--openssl-legacy-provider';
-
-// Ensuite, exécuter le build
-const { execSync } = require('child_process');
-execSync('npm run build', { stdio: 'inherit' });
